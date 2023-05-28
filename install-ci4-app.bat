62fb231061925761de0c0baf5b9f6bed23a95407
abiisaleh <abi.saleh.25@gmail.com>
2023-05-28 17:28:40 +0900

    first commit

---
+++ install-ci4-app.bat
@@ -0,0 +1,17 @@
+git clone http://github.com/abiisaleh/lewi.git
+cd lewi
+cp env .env
+composer udpate
+php spark db:create lewi
+php spark migrate
+php spark db:seed Demo
+php spark auth:create_user admin admin@demo.com
+php spark auth:set_password admin 1234demo
+php spark auth:create_user guru guru@demo.com
+php spark auth:set_password guru 1234demo
+php spark auth:create_user siswa siswa@demo.com
+php spark auth:set_password siswa 1234demo
+php spark auth:create_group admin 'kelola data master'
+php spark auth:create_group guru 'kelola data siswa per kelas'
+php spark auth:create_group siswa 'kelola data pribadi'
+php spark serve
