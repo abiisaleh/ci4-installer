ae6c99fb96b8798c313274716709c5cca03affa4
abiisaleh <abi.saleh.25@gmail.com>
2023-05-29 17:03:15 +0900

    add check dir

--- ci4-launch.bat
+++ ci4-launch.bat
@@ -8,33 +8,34 @@
 ::: 
 :::    wa: 082238204776 | t.me/abiisaleh
 :::  install composer, xampp, git bash dulu
-:::         lakukan install hanya 1x
 :::
 for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A      
 
 set /p repo="Masukkan Kode Repository: "
 
-:install
-set /p choice="Mau apa cok? (install / run / update): "
+IF EXIST "c://xampp/htdocs/%repo%" (
+    :install
+    set /p choice="Mau apa cok? ( run / update): "
 
-if "%choice%"=="install" (
+    if "%choice%"=="run" (
+        cd c://xampp/htdocs
+        cd %repo%
+        start c://xampp/mysql_start.bat & start php spark serve
+        start msedge http://localhost:8080
+    ) else if "%choice%"=="update" (
+        cd c://xampp/htdocs
+        cd %repo%
+        git pull
+    ) else (
+        echo Pilihan salah cok... masukkan "run" atau "update".
+        echo.
+        goto install
+    )
+) ELSE (
     cd c://xampp/htdocs
     git clone http://github.com/abiisaleh/%repo%.git
     cd %repo%
     copy env .env
     copy php.ini c://xampp/php/php.ini
     composer update & call start c://xampp/mysql_start.bat & install-db.bat & start php spark serve & start msedge http://localhost:8080
-) else if "%choice%"=="run" (
-    cd c://xampp/htdocs
-    cd %repo%
-    start c://xampp/mysql_start.bat & start php spark serve
-    start msedge http://localhost:8080
-) else if "%choice%"=="update" (
-    cd c://xampp/htdocs
-    cd %repo%
-    git pull
-) else (
-    echo Pilihan salah cok... masukkan "install" atau "run" atau "update".
-    echo.
-    goto install
 )
