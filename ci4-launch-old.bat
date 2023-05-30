eb7f3a5c5a1bd6301b8ae7729323b4a1e32f653c
abiisaleh <abi.saleh.25@gmail.com>
2023-05-31 08:33:46 +0900

    add versi import sql

---
+++ ci4-launch-old.bat
@@ -0,0 +1,43 @@
+@echo off
+
+:::        _     _ _           _      _     
+:::   __ _| |__ (_|_)___  __ _| | ___| |__  
+:::  / _` | '_ \| | / __|/ _` | |/ _ \ '_ \ 
+::: | (_| | |_) | | \__ \ (_| | |  __/ | | |
+:::  \__,_|_.__/|_|_|___/\__,_|_|\___|_| |_|
+::: 
+:::    wa: 082238204776 | t.me/abiisaleh
+:::  install composer, xampp, git bash dulu
+:::
+for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A      
+
+set /p repo="Masukkan Kode Repository: "
+
+IF NOT EXIST "c://xampp/htdocs/%repo%" (
+    cd c://xampp/htdocs
+    git clone http://github.com/abiisaleh/%repo%.git
+    cd %repo%
+    copy env .env
+    copy php.ini c://xampp/php/php.ini
+    composer update & call start c://xampp/mysql_start.bat & c://xampp/mysql/bin/mysql.exe -u root -p %repo% < file.sql & start php spark serve & start msedge http://localhost:8080 & EXIT
+)
+
+:install
+set /p choice="Mau apa cok? ( run / update): "
+
+IF "%choice%"=="run" (
+    cd c://xampp/htdocs
+    cd %repo%
+    start c://xampp/mysql_start.bat & start php spark serve
+    start msedge http://localhost:8080
+    EXIT
+) ELSE IF "%choice%"=="update" (
+    cd c://xampp/htdocs
+    cd %repo%
+    git pull
+    EXIT
+) ELSE (
+    echo Pilihan salah cok... masukkan "run" atau "update".
+    echo.
+    goto install
+)
